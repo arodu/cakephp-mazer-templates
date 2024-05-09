7baee75f2b7bcdfd59338344d00a4d0c166ae973
Alberto Rodriguez <arodu.dev@gmail.com>
2024-05-09 08:01:13 -0400

    chore: Add MazerTemplates trait for custom view functionality

---
+++ src/View/MazerTemplatesTrait.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace MazerTemplates\View;
+
+/**
+ * MazerTemplates trait
+ */
+trait MazerTemplatesTrait
+{
+
+    /**
+     * @return void
+     */
+    public function MazerTemplatesInitialize(): void
+    {
+    }
+}
