ec33f7cb839058a2b9a840b893de9e18a57b402b
Alberto Rodriguez <arodu.dev@gmail.com>
2025-01-28 13:30:24 -0400

    feat: Enhance MazerTrait by integrating MenuLoaderTrait and updating helper initialization

--- src/View/MazerTrait.php
+++ src/View/MazerTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mazer\View;
 
+use BootstrapTools\View\Trait\MenuLoaderTrait;
 use BootstrapUI\View\UIViewTrait;
 
 /**
@@ -14,6 +15,7 @@ use BootstrapUI\View\UIViewTrait;
 trait MazerTrait
 {
     use UIViewTrait;
+    use MenuLoaderTrait;
 
     /**
      * @return void
@@ -21,38 +23,36 @@ trait MazerTrait
     public function MazerInitialize(array $options = []): void
     {
         $helpers = [
-            'Mazer.Mazer' => $options,
-            'BootstrapTools.Menu' => [],
             'Flash' => [
                 'className' => 'BootstrapUI.Flash',
                 'element' => 'Mazer.flash/default',
             ],
-            'MazerMenu' => [
-                'className' => 'BootstrapTools.Menu',
-                'menuClass' => 'menu',
-                'dropdownClass' => 'has-sub',
-                'templates' => [
-                    'menuContainer' => '<ul class="{{menuClass}}">{{items}}</ul>',
-                    'menuItem' => '<li class="sidebar-item{{class}}{{activeClass}}{{dropdownClass}}">{{text}}{{nest}}</li>',
-                    'menuItemDisabled' => '<li class="sidebar-item{{class}}"><a class="sidebar-link disabled" aria-disabled="true"{{attrs}}>{{icon}}<span>{{text}}</span></a></li>',
-                    'menuItemLink' => '<a class="sidebar-link" href="{{url}}">{{icon}}<span>{{text}}</span>{{append}}</a>',
-                    'menuItemLinkNest' => '<a class="sidebar-link" href="{{url}}">{{icon}}<span>{{text}}</span></a>',
-                    'menuItemDivider' => '<li class="sidebar-item"><hr class="mx-4 my-0"></li>',
-
-                    'dropdownContainer' => '<ul class="submenu">{{items}}</ul>',
-                    'dropdownItem' => '<li class="submenu-item{{class}}{{activeClass}}">{{text}}{{nest}}</li>',
-                    'dropdownItemDisabled' => '<li class="submenu-item{{class}}"><a class="submenu-link disabled" aria-disabled="true"{{attrs}}>{{text}}</a></li>',
-                    'dropdownItemLink' => '<a class="submenu-link" href="{{url}}">{{text}}</a>',
-                    'dropdownItemLinkNest' => '<a class="submenu-link" href="{{url}}">{{text}}</a>',
-                    'dropdownItemDivider' => '<li class="submenu-item"><hr class="mx-4 my-0"></li>',
-
-                    'icon' => '<i class="{{icon}}"></i>',
-                    'menuTitle' => '<li class="sidebar-item {{class}}">{{text}}{{nest}}</li>',
-                ],
-            ],
         ];
         $this->helpers = array_merge($helpers, $this->helpers);
-
         $this->initializeUI();
+
+        $this->loadHelper('Mazer.Mazer', $options);
+        $this->loadMenuHelper('MazerMenu', [
+            'menuClass' => 'menu',
+            'dropdownClass' => 'has-sub',
+            'templates' => [
+                'menuContainer' => '<ul class="{{menuClass}}">{{items}}</ul>',
+                'menuItem' => '<li class="sidebar-item{{class}}{{activeClass}}{{dropdownClass}}">{{text}}{{nest}}</li>',
+                'menuItemDisabled' => '<li class="sidebar-item{{class}}"><a class="sidebar-link disabled" aria-disabled="true"{{attrs}}>{{icon}}<span>{{text}}</span></a></li>',
+                'menuItemLink' => '<a class="sidebar-link" href="{{url}}">{{icon}}<span>{{text}}</span>{{append}}</a>',
+                'menuItemLinkNest' => '<a class="sidebar-link" href="{{url}}">{{icon}}<span>{{text}}</span></a>',
+                'menuItemDivider' => '<li class="sidebar-item"><hr class="mx-4 my-0"></li>',
+
+                'dropdownContainer' => '<ul class="submenu">{{items}}</ul>',
+                'dropdownItem' => '<li class="submenu-item{{class}}{{activeClass}}">{{text}}{{nest}}</li>',
+                'dropdownItemDisabled' => '<li class="submenu-item{{class}}"><a class="submenu-link disabled" aria-disabled="true"{{attrs}}>{{text}}</a></li>',
+                'dropdownItemLink' => '<a class="submenu-link" href="{{url}}">{{text}}</a>',
+                'dropdownItemLinkNest' => '<a class="submenu-link" href="{{url}}">{{text}}</a>',
+                'dropdownItemDivider' => '<li class="submenu-item"><hr class="mx-4 my-0"></li>',
+
+                'icon' => '<i class="{{icon}}"></i>',
+                'menuTitle' => '<li class="sidebar-item {{class}}">{{text}}{{nest}}</li>',
+            ],
+        ]);
     }
 }
