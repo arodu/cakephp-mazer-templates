f278a081a527107809703ff815c438818cdd21d8
Alberto Rodriguez <arodu.dev@gmail.com>
2025-02-06 17:09:22 -0400

    fix: Correct namespace import for ThemeSettingsTrait in MazerHelper

--- src/View/Helper/MazerHelper.php
+++ src/View/Helper/MazerHelper.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Mazer\View\Helper;
 
-use BootstrapTools\View\Helper\ThemeSettingsTrait;
+use BootstrapTools\View\Trait\ThemeSettingsTrait;
 use Cake\View\Helper;
 use Mazer\Mazer;
 
