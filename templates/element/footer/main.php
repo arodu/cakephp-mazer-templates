f46a686a285278e1f931e85eeae588bbc76f222c
Alberto Rodriguez <arodu.dev@gmail.com>
2025-02-07 07:52:30 -0400

    fix: Update footer copyright text formatting in main.php

--- templates/element/footer/main.php
+++ templates/element/footer/main.php
@@ -1,8 +1,8 @@
 <div class="clearfix mb-0 text-muted row">
     <div class="col-12 col-sm-6">
         <p>
-            <?= $this->Bs->copyrightYears(2024) ?>
-            &copy; Cakephp Mazer Template</p>
+            Copyright (c) <?= $this->Bs->copyrightYears(2024) ?> Cakephp Mazer Template
+        </p>
     </div>
     <div class="col-12 col-sm-6 text-sm-end ">
         <p>
