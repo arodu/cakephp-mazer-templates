02a50ff77717efe3e779dbcedfe9fc29438b2a53
Alberto Rodriguez <arodu.dev@gmail.com>
2025-08-19 10:04:12 -0400

    feat: Enhance debug page with structured content and dynamic plugin information

--- templates/Pages/debug.php
+++ templates/Pages/debug.php
@@ -1,1 +1,17 @@
-<?= $this->element('BootstrapTools.debug/page') ?>
+<?php
+
+use UtilityKit\Utility\Common;
+
+$this->assign('title', 'CakePHP: the rapid development php framework');
+$this->assign('subTitle', 'Mazer Templates plugin debug page');
+
+$this->MazerMenu->activeItem('debug');
+
+echo $this->element('BootstrapTools.debug/page', [
+    'plugin' => [
+        'name' => 'arodu/cakephp-mazer',
+        'version' => Common::getPackageVersion('arodu/cakephp-mazer'),
+        'description' => __('A CakePHP plugin for Mazer theme integration.'),
+        'url' => 'https://github.com/arodu/cakephp-mazer',
+    ]
+]);
